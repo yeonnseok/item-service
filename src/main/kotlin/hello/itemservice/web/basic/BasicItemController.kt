e9a6d94710c100e60349e90f881ca9a1a47ae89a
yeonnseok <ljinsk3@gmail.com>
2021-04-20 03:40:29 +0900

    detail item

--- src/main/kotlin/hello/itemservice/web/basic/BasicItemController.kt
+++ src/main/kotlin/hello/itemservice/web/basic/BasicItemController.kt
@@ -5,6 +5,7 @@ import hello.itemservice.domain.item.ItemRepository
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import javax.annotation.PostConstruct
 
@@ -18,6 +19,15 @@ class BasicItemController(
         val items = itemRepository.findAll()
         model.addAttribute("items", items)
         return "basic/items"
+    }
+
+    @GetMapping("/{itemId}")
+    fun item(
+        @PathVariable itemId: Long,
+        model: Model): String {
+        val item = itemRepository.findById(itemId)
+        model.addAttribute("item", item)
+        return "basic/item"
     }
 
     @PostConstruct
